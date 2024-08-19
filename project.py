2e1002cb66f3ec3d5ecc21a403ed4cee411fce88
IT21314742 <it21314742@my.sliit.lk>
2024-08-19 23:16:50 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -11,7 +11,7 @@ while True:
     img = cv2.line(frame, (0, 0), (width, height), (255, 0, 0), 10)
     img = cv2.line(img, (0, height), (width, 0), (0, 255, 0), 5)
 
-    img = cv2
+    img = cv2.
 
 
     cv2.imshow('frame', img)
