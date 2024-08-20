4e086c9c17bfe350ca9cb916d7aa7e88b1fd597d
IT21314742 <it21314742@my.sliit.lk>
2024-08-20 19:25:17 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -14,7 +14,7 @@ while True:
     img = cv2.rectangle(img, (100, 100), (200, 200), (128, 128, 128), 5)
     img = cv2.circle(img, (300, 300), 60, (0, 0, 255), -1)
     font = cv2.FONT_HERSHEY_SIMPLEX
-    img = cv2.putText(img, )
+    img = cv2.putText(img, '')
 
 
 
