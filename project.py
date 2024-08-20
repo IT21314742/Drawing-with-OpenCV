3048861b1ab08b8de831d46224087fdbdc546d54
IT21314742 <it21314742@my.sliit.lk>
2024-08-20 19:32:52 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -9,10 +9,10 @@ while True:
     height = int(cap.get(4))
 
     img = cv2.line(frame, (0, 0), (width, height), (255, 0, 0), 10)
-    # img = cv2.line(img, (0, height), (width, 0), (0, 255, 0), 5)
+    img = cv2.line(img, (0, height), (width, 0), (0, 255, 0), 5)
 
-    # img = cv2.rectangle(img, (100, 100), (200, 200), (128, 128, 128), 5)
-    # img = cv2.circle(img, (300, 300), 60, (0, 0, 255), -1)
+    img = cv2.rectangle(img, (100, 100), (200, 200), (128, 128, 128), 5)
+    img = cv2.circle(img, (300, 300), 60, (0, 0, 255), -1)
     font = cv2.FONT_HERSHEY_SIMPLEX
     img = cv2.putText(img, 'Vihan Jayasinghe', (10, height - 10), font, 2, (0, 0, 0), 5, cv2.LINE_AA)
 
